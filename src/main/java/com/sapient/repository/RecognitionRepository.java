a0e6510f8b99c115d3bbc9e7fe3b10b52d46e435
deepankar <deepankar.d@gmail.com>
2016-06-17 12:14:17 +0530

    Revert "Initial Checkin"
    This reverts commit 7befa473cdb99ee5435c3dbe206fd55a16205f5e.

--- src/main/java/com/sapient/repository/RecognitionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sapient.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecognitionRepository extends JpaRepository<Recognition, Integer> {
-	
-}
